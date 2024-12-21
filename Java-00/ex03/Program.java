34a818c2805671799286c7fbe248d9f1fb678f5a
Ayoub Belhadj <abelhadj@e2r7p9.1337.ma>
2024-12-21 17:49:00 +0100

    java00 ex03

--- Java-00/ex03/Program.java
+++ Java-00/ex03/Program.java
@@ -3,6 +3,40 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+		int countweek = 1;
+		while(countweek <= 18){
+			System.out.print("➜ ");
+			String wek = in.nextLine();
+			if (wek.equals("42"))
+				break;
+			if (!wek.equals("Week "+countweek)){
+				System.out.println("IllegalArgument");
+				in.close();
+				System.exit(-1);
+			}
+			int mingrad = 9;
+			System.out.print("➜ ");
+			for(int i = 0; i < 5; i++){
+				int grad = Integer.parseInt(in.next());
+				if (grad < 1 || grad > 9){
+					System.out.println("IllegalArgument");
+					in.close();
+					System.exit(-1);
+				}
+				mingrad = Math.min(grad, mingrad);
+			}
+			System.out.println(mingrad);
+			in.nextLine();
+			countweek++;
+		}
+		// String ok = "";
+
+		// 	for(int i = 0; i < 5; i++){
+		// 		int j = in.nextInt();
+		// 		ok += "." + Integer.toString(j);
+		// 	}
+			
+		// System.out.println(ok);
+		in.close();
 	}
 }
